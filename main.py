b2f729033b8bc4ae8c045994dc3077bd55e75326
Nost01 <nguyen.wnstn@gmail.com>
2025-08-20 20:28:35 -0600

    edits

--- main.py
+++ main.py
@@ -20,10 +20,10 @@ app.add_middleware(
 def get_db_connection():
     try:
         conn = mysql.connector.connect(
-            host=os.getenv("DB_HOST", "localhost"),
-            user=os.getenv("DB_USER", "root"),
-            password=os.getenv("DB_PASSWORD", ""),
-            database=os.getenv("DB_NAME", "nesthealthcentre")
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
         )
         return conn
     except mysql.connector.Error as e:
